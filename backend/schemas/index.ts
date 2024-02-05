fc4f6f340426724d60d6b65a2dd8cd0405b7e1fe
Abhishek Singh <as9284608@gmail.com>
2024-02-05 11:05:47 +0530

    setting up the schemas

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import testimonials from './testimonials'
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+export const schemaTypes = [testimonials, abouts, brands, contact, skills, workExperience, works]
